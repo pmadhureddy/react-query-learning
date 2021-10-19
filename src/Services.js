a0e02a439829308ab3a624d86fc1245e4ef8c4d9
madhu sudhan <pmadhureddy1510@gmail.com>
2021-10-19 22:44:04 +0530

    removed consoles and adde common api

--- src/Services.js
+++ src/Services.js
@@ -1,19 +1,22 @@
 import axios from "axios";
-const fetchPosts = async () => {
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
+const fetchPosts = async (api) => {
+  console.log(api, "api");
+  const { data } = await axios.get(api);
   return data;
 };
-const fetchPost = async (id) => {
-  console.log(id, "id");
-  const { data } = await axios.get(
-    `https://jsonplaceholder.typicode.com/posts/${id}`
+// const fetchPost = async (id) => {
+//   console.log(id, "id");
+//   const { data } = await axios.get(
+//     `https://jsonplaceholder.typicode.com/posts/${id}`
+//   );
+//   return data;
+// };
+const createPost = async (postData) => {
+  const { data } = await axios.post(
+    "https://jsonplaceholder.typicode.com/posts",
+    postData
   );
   return data;
-};
-const createPost = async (data) => {
-  await axios.post("https://jsonplaceholder.typicode.com/posts", data);
 };
 
-export { fetchPosts, createPost, fetchPost };
+export { fetchPosts, createPost };
